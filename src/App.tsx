e8116ff9fca232e46e5073f3e1cfd8b5eec54e5b
Ellias Ribeiro <elliasribeiro99@gmail.com>
2025-09-20 22:56:36 -0300

    feat(App): add utility functions and button component with Tailwind CSS integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 export function App() {
-  return <></>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
