69699e68b393f7e48642b0a8cfd17e445f9a314c
Ellias Ribeiro <elliasribeiro99@gmail.com>
2025-09-20 23:25:01 -0300

    feat(App): integrate ThemeProvider for theme management and update button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
 
 export function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+    <ThemeProvider>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button onClick={() => alert("Button clicked!")}>Click me</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
