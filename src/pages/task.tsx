b5af6ab73dc64d4f8bc46f1d796ef3382d447743
Ellias Ribeiro <elliasribeiro99@gmail.com>
2025-09-21 00:02:32 -0300

    feat(hooks): add custom hooks for task management with API integration

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -1,3 +1,7 @@
+import { useListTasks } from "@/hooks/queries/use-list-tasks";
+
 export function Tasks() {
+  const { data } = useListTasks();
+  console.log(data);
   return <div>Tasks Page</div>;
 }
